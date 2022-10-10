d5512d0d257709eec8ecc17332448cf50f5e46b4
Nvsprasanna <neerajarongali@Neerajas-MacBook-Air.local>
2022-10-10 18:02:31 +0530

    adding pipeline files

---
+++ ansiblebuildpipe.groovy
@@ -0,0 +1,40 @@
+//Declarative pipeline
+
+pipeline {
+    agent any
+    parameters { 
+                  string(name: 'MYSOURCE_BRANCH', defaultValue: 'master', description: '')
+                  choice(name: 'myip', choices: ['QA,172.31.21.70','Dev,172.31.19.175'], description: 'select IP Value') 
+               }
+    
+    stages {
+        stage('checkout Stage') {
+
+            steps {
+            checkout([$class: 'GitSCM', branches: [[name: '*/$MYSOURCE_BRANCH']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/Nvsprasanna/Sep22-code.git']]])
+                sh '''ls -la'''
+
+            }
+        }
+        stage('Build stage') {
+            steps {
+                echo 'we are in builld statge'
+                sh ''' mvn clean package 
+                ls -la target/ '''
+
+            }
+        }
+        stage('Upload stage') {
+            steps {
+                echo 'we are in upload statge'
+                sh ''' aws s3 cp target/hello-*.war s3://nvsbucket/$JOB_NAME/$MYSOURCE_BRANCH/$BUILD_NUMBER/ '''
+            }
+        }
+        stage('Artifcat check') {
+            steps {
+                echo 'we are in  statge'
+                sh ''' aws s3 ls s3://nvsbucket/$JOB_NAME/$MYSOURCE_BRANCH/$BUILD_NUMBER/ '''
+            }
+        }
+          
+}
